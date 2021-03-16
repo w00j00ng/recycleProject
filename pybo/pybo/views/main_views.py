8b95e7dea3cd9645b4add5ab9452237b75e6037f
w00j00ng <w00j00ng351@gmail.com>
2021-03-16 17:19:20 +0900

    add func upload image

--- pybo/pybo/views/main_views.py
+++ pybo/pybo/views/main_views.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        print("request: ", request.form)
+
+        return render_template('classifier.html')
+
     return render_template('index.html')
