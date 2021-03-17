9fd57cc12db40143d8cbbb561d73b5ed8c6179c6
w00j00ng <w00j00ng351@gmail.com>
2021-03-17 15:52:39 +0900

    Revert "error fixed_when no image is uploaded"
    This reverts commit e137239b8534576763fb9a2807741f55c9932980.

--- pybo/pybo/views/main_views.py
+++ pybo/pybo/views/main_views.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request
 from pybo.classify import clf
 from pybo.config import homedir
-from pybo.forms import UploadImageForm
 import os
 
 bp = Blueprint('main', __name__, url_prefix='/')
@@ -9,8 +8,7 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    form = UploadImageForm()
-    return render_template('index.html', form=form)
+    return render_template('index.html')
 
 
 @bp.route('/guide/', methods=['GET', 'POST'])
