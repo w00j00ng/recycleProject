4f724dde66cc328a4181dd238d800b16703f1b9e
w00j00ng <w00j00ng351@gmail.com>
2021-03-17 16:07:25 +0900

    error_fixed_when no image is uploaded 1

--- pybo/pybo/views/main_views.py
+++ pybo/pybo/views/main_views.py
@@ -2,13 +2,24 @@ from flask import Blueprint, render_template, request
 from pybo.classify import clf
 from pybo.config import homedir
 import os
+from pybo.forms import PhotoForm
+from werkzeug.utils import secure_filename
+
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = PhotoForm()
+    if form.validate_on_submit():
+        f = form.photo.data
+        filename = secure_filename(f.filename)
+        fdir = homedir + filename
+        f.save(fdir)
+        data = clf(fdir)
+        return render_template('guide_' + data + '.html', data=data)
+    return render_template('index.html', form=form)
 
 
 @bp.route('/guide/', methods=['GET', 'POST'])
@@ -20,3 +31,5 @@ def classifier():
     os.remove(fdir)
 
     return render_template('guide_' + data + '.html', data=data)
+
+
